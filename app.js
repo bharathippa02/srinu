50b7d7b5d38f38c8c722f8bdc7a7b03dbec73ae8
Ippa Bharath <bharathippa02@gmail.com>
2021-05-18 10:59:27 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,31 @@
+var video;
+var stopVideo = document.querySelector("#stop");
+function startCamera()
+{
+     video = document.querySelector("#videoElement");
+if (navigator.mediaDevices.getUserMedia) {
+  navigator.mediaDevices.getUserMedia({ video: true })
+    .then(function (stream) {
+      video.srcObject = stream;
+    })
+    .catch(function (err0r) {
+      console.log("Something went wrong!");
+    });
+}
+}
+
+document.getElementById("start").onclick=startCamera;
+
+stopVideo.addEventListener("click", stop, false);
+
+function stop(e) {
+  var stream = video.srcObject;
+  var tracks = stream.getTracks();
+
+  for (var i = 0; i < tracks.length; i++) {
+    var track = tracks[i];
+    track.stop();
+  }
+
+  video.srcObject = null;
+}
